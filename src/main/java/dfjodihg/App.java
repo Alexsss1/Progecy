70218afdb6885b97b66a6e75c43657faa307cfe4
alexandra <Zolotayaalexs@mail.ru>
2015-12-24 20:45:21 +0200

    Initial commit

---
+++ src/main/java/dfjodihg/App.java
@@ -0,0 +1,13 @@
+package dfjodihg;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
